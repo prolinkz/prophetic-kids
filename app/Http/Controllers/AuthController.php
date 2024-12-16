8f5a45fc591f18d80a9304cd8e1298fe4ec703f0
Zakiullah Khan <prolinkzee@gmail.com>
2024-12-17 04:21:55 +0500

    Add login page
    1. Loaded Bulona Theme assests.
    2. Create the AuthController for Login.
    3. Set the Login page access

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // echo "Hello"; die();
+        return view('auth.login');
+    }
+}
